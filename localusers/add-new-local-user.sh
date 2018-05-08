63ae0c594d72b4a2908aaa53df609efa432b8f14
David S. Jackson <deepbsd@yahoo.com>
2018-05-08 00:44:14 -0400

    working on exercise 3 for bash class

--- localusers/add-new-local-user.sh
+++ localusers/add-new-local-user.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-
-
-
-#!/usr/bin/env bash
-
 # User must be executing with root privileges; if not, exit 1
 if [[ "${UID}" -eq 0 ]]
 then
@@ -16,25 +11,23 @@ fi
 
 # If the user doesn't supply at least one argument, then give them help.
 NUMBER_OF_PARAMETERS="${#}"
-if [[ ${NUMBER_OF_PARAMETERS} -lt 1 ]]
+if [[ ${NUMBER_OF_PARAMETERS} -lt 2 ]]
 then
   echo "Usage: ${0} USER_NAME [FULL NAME]..."
   exit 1
 fi
 
-# Prompts user to enter username (login)
-read -p 'Enter the username to create: ' USER_NAME
+# The first parameter is the USER_NAME
+USER_NAME=${1}
+
+# The rest of the parameters are for the account comments.
+COMMENT=${@:2}
+
+# Generate a password.
+PASSWORD=$(date +%s%N | sha256sum | head -c8)
 
 
-# Prompts user to enter full name for person using account (description)
-read -p 'Enter the name of the person who this account is for: ' COMMENT
-
-
-# Prompts user for initial password for the account
-read -p 'Enter the initial password: ' PASSWORD
-
-
-# Creates a new user on the local system with this input from user
+# Creates a new user on the local system 
 useradd -c "${COMMENT}" -m ${USER_NAME}
 
 
@@ -90,20 +83,5 @@ exit 0
 
 
 
-# The first parameter is the user name.
 
-# The rest of the parameters are for the account comments.
 
-# Generate a password.
-
-# Create the user with the password.
-
-# Check to see if the useradd command succeeded.
-
-# Set the password.
-
-# Check to see if the passwd command succeeded.
-
-# Force password change on first login.
-
-# Display the username, password, and the host where the user was created.
